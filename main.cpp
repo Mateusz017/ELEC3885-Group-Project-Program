bf90274ffc78a82c7be1e915fbd84ef574a52f9c
Mateusz017 <el14ml@leeds.ac.uk>
2017-05-04 12:08:20 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,130 +1,152 @@
 /*
-* @file main.cpp
-* @Group Design Project
-* @brief Main file containing functions and int main().
-* @author Mateusz Loboda
-* @date April 2017
+
+Copyright (c) 2012-2014 RedBearLab
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 */
 
-#include "main.h"
+/*
+ *    The application works with the BLEController iOS/Android App.
+ *    Type something from the Terminal to send
+ *    to the BLEController App or vice verse.
+ *    Characteristics received from App will print on Terminal.
+ */
+ 
+#include "mbed.h"
+#include "ble/BLE.h"
 
-int main()
+
+#define BLE_UUID_TXRX_SERVICE            0x0000 /**< The UUID of the Nordic UART Service. */
+#define BLE_UUID_TX_CHARACTERISTIC       0x0002 /**< The UUID of the TX Characteristic. */
+#define BLE_UUIDS_RX_CHARACTERISTIC      0x0003 /**< The UUID of the RX Characteristic. */
+
+#define TXRX_BUF_LEN                     20
+
+BLE  ble;
+
+//Serial pc(USBTX, USBRX);
+Serial device(P0_9, P0_11);  // tx, rx
+
+// The Nordic UART Service
+static const uint8_t uart_base_uuid[] = {0x71, 0x3D, 0, 0, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
+static const uint8_t uart_tx_uuid[]   = {0x71, 0x3D, 0, 3, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
+static const uint8_t uart_rx_uuid[]   = {0x71, 0x3D, 0, 2, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
+static const uint8_t uart_base_uuid_rev[] = {0x1E, 0x94, 0x8D, 0xF1, 0x48, 0x31, 0x94, 0xBA, 0x75, 0x4C, 0x3E, 0x50, 0, 0, 0x3D, 0x71};
+
+
+uint8_t txPayload[TXRX_BUF_LEN] = {0,};
+uint8_t rxPayload[TXRX_BUF_LEN] = {0,};
+
+static uint8_t rx_buf[TXRX_BUF_LEN];
+static uint8_t rx_len=0;
+
+
+GattCharacteristic  txCharacteristic (uart_tx_uuid, txPayload, 1, TXRX_BUF_LEN, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE_WITHOUT_RESPONSE);
+                                      
+GattCharacteristic  rxCharacteristic (uart_rx_uuid, rxPayload, 1, TXRX_BUF_LEN, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY);
+                                      
+GattCharacteristic *uartChars[] = {&txCharacteristic, &rxCharacteristic};
+
+GattService         uartService(uart_base_uuid, uartChars, sizeof(uartChars) / sizeof(GattCharacteristic *));
+
+
+
+void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params)
 {
-    temperature = tmp102.read_temperature(); // read base temperature
-    pc.printf("\nBASE T = %.2f ", temperature); // print to cool term
-    tmp102.low_threshold(temperature + 0.5F); //setting low temperature threshold
-    tmp102.high_threshold(temperature + 1); //setting high temperature threshold
-    device.attach(&device_Rx,Serial::RxIrq); //serial interrupt to receive data when it is sent
-    tmp102.init(); // initialise TMP102 temperature sensor
-    pc.baud(9600);
-    device.baud(9600); //make sure baud rate matches cool term setting
-
-    while(1) {
-
-        sleep();
-    }
+    //device.printf("Disconnected \r\n");
+    //device.printf("Restart advertising \r\n");
+    ble.startAdvertising();
 }
 
-void device_Rx()
-{
-    char buff[50];     //array for any data received over UART
-    char str[20];      //array for debugging so can print received messages in coolterm
-    if (device.readable()) {   //if BLE is sending any data
-
-        char operation = device.getc();  //get the data and store in operation
-        buff[0]=operation;               //stores first character of data in buffer
-
-        if (operation == '?') {          //if ? character received the next character refers to drug chamber
-            chamber = device.getc();     //get chamber number i.e. type of drug
-            chamber_int = chamber - 48;  //convert the ascii character obtained over UART to integer
-            stepperRx = 1;               //set Bool to 1 for drug delivery
-            buff[1]=chamber;             //for cool term feedback
-            sprintf(str,"%c",buff[1]);   //print chamber number
-            pc.printf(str);
-        } else if (operation == '!') {   //if K64F receives ! the next character refers to the amount of drug that will be dispensed
-
-            drugAmount = device.getc();  //receives drug amount character 1,2,3,4 corresponding to 25%, 50%, 75%, 100&%
-            actuatorRx = 1;              //set actuator Bool to 1
-            buff[1]=drugAmount;          //for cool term feedback
-            sprintf(str,"%c",buff[1]);
-            pc.printf(str);
-            drugAmount_int = drugAmount - 48; //convert ascii character obtained via UART to integer
-            char sendString[6];
-            sprintf(sendString,"?%d!%d00",chamber_int,drugAmount_int);
-            device.puts(sendString);     //echo the message back to the app and wait for confirmation
-            pc.printf(sendString);       //print the drug delivery message
-        } else if (operation == 'o') {   //confirmation received, the message has been received correctly
-            pc.printf("ok");
-            delivery();                  // deliver the drug
-        } else if (operation == 't') {   // app is asking for temp feedback
-            temperature = tmp102.read_temperature(); // read temperature from sensor
-            char feedback[6];
-            sprintf(feedback,"t%.2f",temperature);
-            device.puts(feedback);       // send the temperature string to the app
-        } else if (operation == 'd') {   // app is asking for feedback of each drug chamber
-            char message[6];
-            sprintf(message,"d%d%d%d00",drugAmountFeedback,drugAmountFeedback2,drugAmountFeedback3); //drug amounts are represented with integers 1-4, one for each chamber
-            device.puts(message);        // send to the app
-        } else if (operation == 'h') {   // app is requesting threshold temperature feedback
-            int feedbackAlert = tmp102.alert_status();  // obtain the threshold flag
-            char message[6];
-            sprintf(message,"h%d0000",feedbackAlert);
-            device.puts(message);
-            pc.printf("feedback temperature"); //send the threshold feedback
-
+void WrittenHandler(const GattWriteCallbackParams *Handler) //app to ble to mcu 
+{   
+    uint8_t buf[TXRX_BUF_LEN];
+    uint16_t bytesRead, index;
+    
+    if (Handler->handle == txCharacteristic.getValueAttribute().getHandle()) 
+    {
+        ble.readCharacteristicValue(txCharacteristic.getValueAttribute().getHandle(), buf, &bytesRead); //app to ble
+        memset(txPayload, 0, TXRX_BUF_LEN);
+        memcpy(txPayload, buf, TXRX_BUF_LEN);       
+        //device.printf("WriteHandler \r\n");
+        //device.printf("Length: ");
+        device.putc(bytesRead);  //device is ble puts msg from app to mcu
+        //device.printf("\r\n");
+        //device.printf("Data: ");
+        for(index=0; index<bytesRead; index++)
+        {
+            device.putc(txPayload[index]);        
         }
-
+        //device.printf("\r\n");
     }
 }
-
-void delivery()
-{
-    pc.printf("chamber\n");
-    pc.printf("%i, %i,%i", stepperRx, actuatorRx); //printing Bools
-
-    if (stepperRx == 1 && actuatorRx == 1) {   //checks if drug type and amount has been received
-
-        current_drug_chamber = stepper.rotateChamber(chamber_int);  //rotate now if 1 do nothing, if 2 do 120 degrees if 3 240 degrees then return to chamber 1
-        pc.printf("FORWARDS\n"); // print to serial connection
-        pc.printf("%i", drugAmount_int); //expected amount to be released
-        if (chamber_int == 1) {  //if chamber_int = 1 release drug from LA 1
-            actuator.actuate(drugAmount_int*8); // actuates a distance of 37 mm -40 to 40m
-            wait(2); // delay of 2 seconds during release
-            pc.printf("BACKWARDS\n"); // print to serial connection
-            actuator.actuate(drugAmount_int*(-8));  //retracting by the same amount using negative value -10
-            drugAmountFeedback = drugAmountFeedback - drugAmount_int; //100% - amount released
-            deliveryFeedback(); //send feedback to the app
-
-        } else if (chamber_int == 2) { //if chamber_int = 2 release drug from LA 2
-            actuator2.actuate(drugAmount_int*8);
-            wait(2); // delay of 5 seconds
-            pc.printf("BACKWARDS\n"); // print to serial connection
-            actuator2.actuate(drugAmount_int*(-8)); //backwards by x mm   //retracting by the same amount using negative value
-            drugAmountFeedback2 = drugAmountFeedback2 - drugAmount_int;
-            deliveryFeedback();
-        } else if (chamber_int == 3) {  //if chamber_int = 3 release drug from LA 3
-            actuator3.actuate(drugAmount_int*8);
-            wait(2); // delay of 2 seconds
-            pc.printf("BACKWARDS\n"); // print to serial connection
-            actuator3.actuate(drugAmount_int*(-8)); //backwards by x mm   //retracting by the same amount using negative value
-            drugAmountFeedback3 = drugAmountFeedback3 - drugAmount_int;
-            deliveryFeedback(); //this sends feedback before chamber is retracted
+void uartCB(void) //mcu to ble to app 
+{   
+    while(device.readable())    
+    {
+        rx_buf[rx_len++] = device.getc();    //get msg from mcu as soon as its 6 characters 
+        if(rx_len>=6 || rx_buf[rx_len-1]=='\0' || rx_buf[rx_len-1]=='\n')  //when 6 characters are stored in buffer it sends the msg to ble
+        {
+            ble.updateCharacteristicValue(rxCharacteristic.getValueAttribute().getHandle(), rx_buf, rx_len); //this changes characteristic value which is updated in app
+            //device.printf("RecHandler \r\n");
+            //device.printf("Length: ");
+            device.putc(rx_len); 
+            //device.printf("\r\n");
+            rx_len = 0;
+            break;
         }
-
-        pc.printf("ROTATE STEPPER MOTOR TO CHAMBER 1\n"); // print to serial connection
-        current_drug_chamber = stepper.rotateChamber(CHAMBER_1);  //can return to drug chamber 1 at the end for reference
-        wait(2); // delay of 2 seconds
-        stepperRx = 0; //resetting both bools before the next command is received
-        actuatorRx = 0;
-        pc.printf("%i, %i", stepperRx, actuatorRx); //printing Bools
-
     }
-
 }
-void deliveryFeedback()
+int main(void)
 {
-    char sendString[6];
-    sprintf(sendString,"s00000"); // send this as feedback when drug delivery is complete
-    device.puts(sendString);
-    pc.printf(sendString);
+    ble.init(); //initialize ble protocol stack
+    ble.onDisconnection(disconnectionCallback);
+    ble.onDataWritten(WrittenHandler);  
+    
+    //pc.baud(9600);
+    device.baud(9600);
+    //device.printf("SimpleChat Init \r\n");
+    
+    device.attach( uartCB , device.RxIrq); //event interrupt wakes up ble when smth is sent to mcu or received from mcu
+   // setup advertising 
+    ble.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED); 
+    ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
+    ble.accumulateAdvertisingPayload(GapAdvertisingData::SHORTENED_LOCAL_NAME,
+                                    (const uint8_t *)"Biscuit", sizeof("Biscuit") - 1);
+    ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_128BIT_SERVICE_IDS,
+                                    (const uint8_t *)uart_base_uuid_rev, sizeof(uart_base_uuid));
+    // 100ms; in multiples of 0.625ms. 
+    ble.setAdvertisingInterval(160);
+    //once connected to peripheral stop advertising
+
+    ble.addService(uartService); //exchanges data service which reads or writes to ble to communicate in both directions
+    //assign service, ble wakes up otherwsie sleeps to save power
+    
+    ble.startAdvertising(); 
+    //device.printf("Advertising Start \r\n");
+    
+    while(1)
+    {
+        ble.waitForEvent(); 
+    }
 }
+
+
+
+
+
+
+
+
